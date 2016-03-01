a38f7240ddfb750590900067d6a2ba83edc36a6f
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-01 03:06:32 -0800

    description and copyright changed

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-lmdb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure bindings for lmdb"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
