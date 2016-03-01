fe39388032a8b365377420e751cb11d0f690f1e9
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-01 03:05:15 -0800

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-lmdb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_lmdb/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-lmdb.core-test
+  (:require [clojure.test :refer :all]
+            [clj-lmdb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
