d84f2b4111dcac24043c2a5bd0c893dfb1bb96d8
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-22 23:57:17 -0700

    added deps

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://github.com/shriphani/clj-lmdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.deephacks.lmdbjni/lmdbjni "0.4.6"]
+                 [org.deephacks.lmdbjni/lmdbjni-linux64 "0.4.6"]
+                 [org.deephacks.lmdbjni/lmdbjni-win64 "0.4.6"]
+                 [org.deephacks.lmdbjni/lmdbjni-osx64 "0.4.6"]])
