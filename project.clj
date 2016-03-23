5c6bf3d3d9f759725cf80768d32cd49ed1fba259
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-22 23:45:01 -0700

    clj 1.8

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-lmdb "0.1.0-SNAPSHOT"
   :description "Clojure bindings for lmdb"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/shriphani/clj-lmdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
